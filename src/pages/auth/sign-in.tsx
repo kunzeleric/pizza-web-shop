baa91c4f095f6d5b81b9a86cba199d7892148774
Eric Kunzel <kunzel.eric@gmail.com>
2024-07-14 09:18:46 -0300

    feat: installed packages, started sign in page layout, created toast in main app and installed label and input components

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,54 @@
-import { Helmet } from 'react-helmet-async'
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { z } from "zod";
+import { toast } from "sonner";
+
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SigninForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SigninForm>();
+
+  function handleSignIn(data: SigninForm) {
+    toast.success("Enviamos um link de autenticação para seu e-mail.", {
+      action: {
+        label: "Reenviar",
+        onClick: () => handleSignIn(data)
+      }
+    })
+  }
+
   return (
     <>
       <Helmet title="Login" />
-      <h1>SignIn</h1>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar o painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel de controle
+            </p>
+          </div>
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register("email")} />
+            </div>
+          </form>
+          <Button disabled={isSubmitting} type="submit" className="w-full">
+            Acessar Painel
+          </Button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
