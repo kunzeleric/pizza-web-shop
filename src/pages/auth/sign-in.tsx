4e28d1cef8a53ff9400f76d3357661cfaf2fe3d3
Eric Kunzel <kunzel.eric@gmail.com>
2024-07-15 09:01:37 -0300

    fix: included signup button in signin page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,27 +1,32 @@
-import { Helmet } from "react-helmet-async";
-import { useForm } from "react-hook-form";
+import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { z } from "zod";
-import { toast } from "sonner";
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 
 const signInForm = z.object({
   email: z.string().email(),
-});
+})
 
-type SigninForm = z.infer<typeof signInForm>;
+type SigninForm = z.infer<typeof signInForm>
 
 export function SignIn() {
-  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SigninForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SigninForm>()
 
   function handleSignIn(data: SigninForm) {
-    toast.success("Enviamos um link de autenticação para seu e-mail.", {
+    toast.success('Enviamos um link de autenticação para seu e-mail.', {
       action: {
-        label: "Reenviar",
-        onClick: () => handleSignIn(data)
-      }
+        label: 'Reenviar',
+        onClick: () => handleSignIn(data),
+      },
     })
   }
 
@@ -29,6 +34,9 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -41,7 +49,7 @@ export function SignIn() {
           <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" {...register("email")} />
+              <Input id="email" type="email" {...register('email')} />
             </div>
           </form>
           <Button disabled={isSubmitting} type="submit" className="w-full">
@@ -50,5 +58,5 @@ export function SignIn() {
         </div>
       </div>
     </>
-  );
+  )
 }
