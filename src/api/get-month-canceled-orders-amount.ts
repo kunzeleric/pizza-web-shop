3a471d30d8970f4b82f97fad29febb755c665028
Eric Kunzel <kunzel.eric@gmail.com>
2024-08-07 09:04:20 -0300

    feat: created logic for metric cards in dashboard and implemented them + force user logout if unauthorized

---
+++ src/api/get-month-canceled-orders-amount.ts
@@ -0,0 +1,14 @@
+import { api } from '@/lib/axios'
+
+export interface GetMonthCanceledOrderAmountResponse {
+  amount: number
+  diffFromLastMonth: number
+}
+
+export async function getMonthCanceledOrderAmount() {
+  const response = await api.get<GetMonthCanceledOrderAmountResponse>(
+    '/metrics/month-canceled-orders-amount',
+  )
+
+  return response.data
+}
