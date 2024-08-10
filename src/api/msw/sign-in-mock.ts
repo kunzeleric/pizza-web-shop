68d87585771a6c5c88a115f93d6ca214dba880c4
Eric Kunzel <kunzel.eric@gmail.com>
2024-08-10 10:23:23 -0300

    feat: installed and configured msw and created sign in mock

---
+++ src/api/msw/sign-in-mock.ts
@@ -0,0 +1,13 @@
+import { http, HttpResponse } from 'msw'
+
+export const signInMock = http.post('/authenticate', async ({ request }) => {
+  const { email } = await request.json()
+
+  if (email === 'johndoe@example.com')
+    return new HttpResponse(null, {
+      status: 200,
+      headers: { 'Set-Cookie': 'auth=sample-jwt' },
+    })
+
+  return new HttpResponse(null, { status: 401 })
+})
